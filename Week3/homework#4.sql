936e8304cb98eeb7ad4709b62388751f4626ba3c
Marvin <marvin@gmail.com>
2024-02-10 13:09:58 -0500

    updated sql code for homework

--- Week3/homework#4.sql
+++ Week3/homework#4.sql
@@ -24,3 +24,17 @@ from `ny_taxi.green_taxi_materialized_data`;
 SELECT count(*)
 from `ny_taxi.green_taxi_materialized_data`
 where fare_amount = 0.0
+
+CREATE OR REPLACE TABLE `ny_taxi.green_taxi_partitioned_table`
+PARTITION BY DATE(lpep_pickup_datetime)
+CLUSTER BY PUlocationID AS 
+SELECT * from `ny_taxi.green_taxi_materialized_data`;
+
+
+SELECT distinct PULocationID
+from `ny_taxi.green_taxi_materialized_data`
+where date(lpep_pickup_datetime) between '2022-06-01' and '2022-06-30'
+
+SELECT distinct PULocationID
+from `ny_taxi.green_taxi_partitioned_table`
+where date(lpep_pickup_datetime) between '2022-06-01' and '2022-06-30'
